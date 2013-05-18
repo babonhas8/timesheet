f3fa989c21481151d81bdbbc700dd3963e59a8cc
Mauro Marques <mauro.marques89@gmail.com>
2013-05-18 21:09:27 +0200

    First heroku deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maurotimesheet::Application.config.secret_token = '7f8fcb695c2d47a83ef0825d752245792c22fa772b5da0aec19c8c7f4ac3709078f4b2d2a27a084bfe826b3e6e6bc48c819595e22f069cad453706cc692cd913'
